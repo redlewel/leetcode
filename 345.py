291c30cdff49bcd5efdae19ec23b2f0631e506f7
Eric Lewellen <thesenatesidious@gmail.com>
2024-09-18 02:41:01 -0500

    345: Added, not very hard

---
+++ 345.py
@@ -0,0 +1,22 @@
+
+
+
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowels = 'auieoAEIOU'
+        vowel_list = []
+        for i in enumerate(s):
+            if i[1] in vowels:
+                vowel_list.append(i[1])
+        newstr = ''
+        for i in s:
+            if i in vowels:
+                newstr = newstr + vowel_list.pop()
+            else:
+                newstr = newstr + i
+                
+        return newstr
+
+
+problem = Solution()
+print(problem.reverseVowels("IceCreAm"))
