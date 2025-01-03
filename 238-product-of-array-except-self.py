94e7769290c4895872b9dc2b3b8570f5eb389101
Eric Lewellen <thesenatesidious@gmail.com>
2025-01-02 23:33:42 -0600

    238 finished and working

--- 238-product-of-array-except-self.py
+++ 238-product-of-array-except-self.py
@@ -5,21 +5,29 @@ and https://www.geeksforgeeks.org/suffix-product-array/
 seems according to this video: https://www.youtube.com/watch?v=5bS636lE_R0
 Its more like right and left not exactly full prefix and full suffix.  So I need to remove the first calc on each side. 
 
+Figured it out.  So what was needed is to multiply on the same level on each side.  This is because it's needed to keep in mind that the input 
+and prefix/suffix arrays are still different, so you multiply at the same index to get the result of the following or preceding index when populating
+the prefix and suffix arrays. That's something that tripped me up.  
+Because this is all the left side and right sides multiplied when multiplying across you multiply everything in the array minus the actual value at 
+that index, because that is not included when multiplying from only left or right.  The video explains this more thoroughly. 
+
 """
 from typing import List
 class Solution:
+
+    def prefix(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        prefix_product = [1] * n
+        for i in range(1, n):
+            prefix_product[i] = prefix_product[i - 1] * nums[i - 1] 
+        return prefix_product
+
     def suffix(self, nums: List[int]) -> List[int]:
         n = len(nums)
         suffix_product = [1] * n
-
         for i in range(n - 2, -1, -1):
             suffix_product[i] = suffix_product[i + 1] * nums[i + 1]
         return suffix_product
-
-    def prefix(self, nums: List[int]) -> List[int]:
-        for i in range(1, len(nums)):
-            nums[i] = nums[i] * nums[i - 1]
-        return nums
 
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         suffix_arr = self.suffix(nums)
@@ -30,9 +38,9 @@ class Solution:
 
 
 dog = Solution()
-#print(dog.productExceptSelf([1,2,3,4]))
-print(dog.prefix([1,2,3,4]))
-print(dog.suffix([1,2,3,4]))
+print(dog.productExceptSelf([1,2,3,4]))
+print(dog.prefix([1,2,3,4])) # This should be [1, 1, 2, 6]
+print(dog.suffix([1,2,3,4])) # This should be [24,12, 4, 1]
 
 #dog.productExceptSelf([-1,1,0,-3,3])
 
