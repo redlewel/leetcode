243bccbac6ec42dded93b652bd221ce0ccac8638
Eric Lewellen <elewellen@hivelocity.net>
2025-01-25 02:53:03 -0600

    88: truly awful attempt at beating edge cases, it is too late for this.  Will try again later

--- 88-merge-sorted-array.py
+++ 88-merge-sorted-array.py
@@ -2,23 +2,28 @@ from typing import List
 
 class Solution:
 	def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-		place = 0
+		if len(nums1) < 2:
+			if nums1[0] == 0:
+				nums1[0] = nums2[0]
+		if len(nums1) < 3:
+			if nums1[1] == 0:
+				nums1[1] = nums2[0]
+		place = len(nums1) - 1
 		for i in range(0, n):
-			print("dog")
-			for a in range((len(nums1) - 1), 0, -1):
-				print("cat")
+			for a in range(place, 0, -1):
 				if nums1[a] == 0:
 					continue
 				if nums2[i] >= nums1[a]:
-					print(f"current list: {nums1}")
-					print(f"Nums2[i]: {nums2[i]} Nums1[a]: {nums1[a]}")
+					print(f"List before: {nums1}")
+					print(f"nums2[i]: {nums2[i]}, nums1[a]: {nums1[a]}")
 					nums1.insert(a + 1, nums2[i])
-					place = i + 1
+					place = a + 1
 					nums1.pop()
-					print(f"list after: {nums1}")
+					print(f"List after: {nums1}")
 					break
 		print(nums1)
 
 solve = Solution()
 solve.merge([1, 2, 4, 0, 0, 0], 3, [2, 5, 6], 3)
+solve.merge([1, 0], 1, [2], 1)
 
