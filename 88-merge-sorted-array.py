c6cb1d9c81ed46e61cb2a3e68ef26efa7e41f5f8
Eric Lewellen <elewellen@hivelocity.net>
2026-03-06 15:57:37 -0600

    88: non-working version saving progress

--- 88-merge-sorted-array.py
+++ 88-merge-sorted-array.py
@@ -1,29 +1,35 @@
 from typing import List
 
 class Solution:
-	def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-		if len(nums1) < 2:
-			if nums1[0] == 0:
-				nums1[0] = nums2[0]
-		if len(nums1) < 3:
-			if nums1[1] == 0:
-				nums1[1] = nums2[0]
-		place = len(nums1) - 1
-		for i in range(0, n):
-			for a in range(place, 0, -1):
-				if nums1[a] == 0:
-					continue
-				if nums2[i] >= nums1[a]:
-					print(f"List before: {nums1}")
-					print(f"nums2[i]: {nums2[i]}, nums1[a]: {nums1[a]}")
-					nums1.insert(a + 1, nums2[i])
-					place = a + 1
-					nums1.pop()
-					print(f"List after: {nums1}")
-					break
-		print(nums1)
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        if m <= 1:
+            pass
+        else:
+            for i in range(0, n):
+                self.merge_single(nums1, nums2)
 
-solve = Solution()
-solve.merge([1, 2, 4, 0, 0, 0], 3, [2, 5, 6], 3)
-solve.merge([1, 0], 1, [2], 1)
+
+    @staticmethod 
+    def merge_single(nums1: List[int], nums2: List[int]):
+        for i2, n2 in enumerate(nums2): 
+            for i1, n1 in enumerate(nums1): 
+                if n1 > n2:
+                    print(f"Current Nums1: {nums1}")
+                    step_place = i1 + 1
+                    temp = n1
+                    nums1[i1] = n2
+                    if nums1[step_place] == 0: # If next index is 0 then jump to next n
+                        nums1[step_place] = temp
+                        continue
+                    while nums1[step_place] != 0:
+                        step_temp = nums1[step_place]
+                        nums1[step_place] = temp
+                        temp = step_temp
+                        step_place += 1
+                        
+
+
 
